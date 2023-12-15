e3e01ec9343d3611cd88ef232313f5755a604298
Olha <vasylieva.hell@gmail.com>
2023-12-15 10:54:29 +0200

    new

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = '6813211270:AAGMzQjEhmOP36WpYi-97Xih3N1ZYpvfcNM'
 APP_URL = 'https://admintelegrambot-663983b44628.herokuapp.com/' + BOT_TOKEN
-DB_URI = 'postgres://lwqtzglycczwpk:504de6af550af6d09f4763d9f42783fb9b0d5f01b6e05814ed59d0c3b1ba3223@ec2-34-247-16-250.eu-west-1.compute.amazonaws.com:5432/d86j98i2m1bioj'
+DB_URI = 'postgres://heftcjrklsfgww:69a65a2996903002e34a6dcf36165cf0329477daefcd0756f79e4f8661c12257@ec2-54-73-22-169.eu-west-1.compute.amazonaws.com:5432/dcvk3us4mk7eao'
