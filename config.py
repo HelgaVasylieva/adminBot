79352cd19248bde1a4d3bfd7bffeddba7c885b1d
Olha <vasylieva.hell@gmail.com>
2023-12-15 12:41:11 +0200

    new

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = '6813211270:AAGMzQjEhmOP36WpYi-97Xih3N1ZYpvfcNM'
 APP_URL = 'https://admintelegrambot-663983b44628.herokuapp.com/' + BOT_TOKEN
-DB_URI = 'postgres://heftcjrklsfgww:69a65a2996903002e34a6dcf36165cf0329477daefcd0756f79e4f8661c12257@ec2-54-73-22-169.eu-west-1.compute.amazonaws.com:5432/dcvk3us4mk7eao'
+DB_URI = 'postgresql://ec2-54-73-22-169.eu-west-1.compute.amazonaws.com:5432/dcvk3us4mk7eao'
